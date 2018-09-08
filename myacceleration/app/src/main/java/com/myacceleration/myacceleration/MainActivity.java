0dde0b7dbed4d3f73f72ee96844361e4f9df8a2b
Robert <206040@edu.p.lodz.pl>
2018-09-08 02:37:18 +0200

    poprawki pomiarow/ wersja heroku

--- myacceleration/app/src/main/java/com/myacceleration/myacceleration/MainActivity.java
+++ myacceleration/app/src/main/java/com/myacceleration/myacceleration/MainActivity.java
@@ -16,7 +16,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -28,36 +27,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.myacceleration.myacceleration.db.AppDatabase;
 import com.myacceleration.myacceleration.db.Car;
-import com.myacceleration.myacceleration.db.User;
+import com.myacceleration.myacceleration.db.CarRepository;
 
 import java.sql.Timestamp;
-import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
     public static final int MY_PREMISSION_LOCALIZATION = 10;
-    public static String SERVER = "http://192.168.1.23:8080/";
+    private static final float KM_H_FAKTOR = 3.6f;
+    //public static String SERVER = "http://192.168.1.23:8080/";
     //public static String SERVER = "http://192.168.43.13:8080/";
-    //public static String SERVER = "https://acctest33.herokuapp.com/";
+    public static String SERVER = "https://acctest33.herokuapp.com/";
     private static String TAG  = "myacceleration_MainActivity";
     private ToggleButton startBtn;
     private Button speedUpBtn, speedDownBtn;
-    private TextView t;
     private TextView tvCarName;
-    private TextView s,t1,t2,res,max;
+    private TextView s, time, res, max;
     private LocationManager locationManager;
     private LocationListener listener;
-    private float maxSpeed = 15;
-    private long resultAcc = 0;
-    private int noResults = 0;
+    private float maxSpeed = 60;
     private boolean record;
+    private ArrayList<Float> results = new ArrayList();
 
-    private long timer1,timer2;
+    private long timer1;
     private String mUsername = "";
 
     private static float fakegpsValue = 0.0f;
+    private LoadInitialDataTask mcarTask;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -69,55 +67,66 @@ public class MainActivity extends AppCompatActivity
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        t = (TextView) findViewById(R.id.textView);
         startBtn = (ToggleButton) findViewById(R.id.startBtn);
         speedUpBtn = (Button) findViewById(R.id.speedUpBtn);
         speedDownBtn = (Button) findViewById(R.id.speedDownBtn);
 
         s = (TextView) findViewById(R.id.textView2);
-
-        t1 = (TextView) findViewById(R.id.textView4);
-        t2 = (TextView) findViewById(R.id.textView5);
+        time = (TextView) findViewById(R.id.textView4);
         res = (TextView) findViewById(R.id.textView6);
         max = (TextView) findViewById(R.id.textView7);
         tvCarName = (TextView) findViewById(R.id.car_name);
+
+        mcarTask = new LoadInitialDataTask();
+        mcarTask.execute((Void) null);
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                final long timestamp = System.currentTimeMillis();
                 // dla testow zakomentowane
-                //float lSpeed = location.getSpeed();
+                //float lSpeed = location.getSpeed() * KM_H_FAKTOR;
                 // dla testow losujemy predkosc:
-                float lSpeed = fakeGPS();
-                Log.d(TAG, "speed changed:"+lSpeed);
+                float lSpeed = fakeGPS() * KM_H_FAKTOR;
+                s.setText( round1Digit(lSpeed)+ " km/h");
 
-                t.setText("\n " + location.getLongitude() + "\n" + location.getLatitude());
-                s.setText( lSpeed + "m/s");
-                if (lSpeed <= 0.5)
+                if (lSpeed <= 3.0f)
                 {
                     record = true;
-                    timer1 = timestamp.getTime();
-                    timer2 = 0;
-                    t1.setText("t1: " + timer1);
-                    t2.setText("t2: " + timer2);
+                    timer1 = timestamp;
                 }
+
+                float resultAccSeconds = (timestamp - timer1)/1000;
+                if (record == true) {
+                    time.setText("Czas: " + round3Digit(resultAccSeconds));
+                } else {
+                    time.setText("Zwolnij aby zacząć kolejną rundę");
+                }
+                Log.d(TAG, "speed changed:"+lSpeed + " | max:" +maxSpeed+ " | record:" +record + " | sec"+resultAccSeconds);
 
                 if (lSpeed >= maxSpeed && record == true)
                 {
                     record = false;
-                    timer2 = timestamp.getTime();
-                    t2.setText("t2: " + timer2);
-                    resultAcc = timer2 - timer1;
-                    noResults++;
-                    res.append("\n[" + noResults + "]: " + resultAcc);
+                    results.add(round3Digit(resultAccSeconds));
+                    res.setText(displayResults(results));
+                    timer1 = timestamp;
                 }
             }
 
+            private CharSequence displayResults(ArrayList<Float> results) {
+                int no = 0;
+                String text = "Wyniki:";
+                for(Float result : results) {
+                    no++;
+                    text += "\n" + no + ". " + round1Digit(result) + " s";
+                }
+                return text;
+            }
+
             private float fakeGPS() {
-                if(fakegpsValue > maxSpeed) fakegpsValue = 0.0f;
+                if(fakegpsValue > maxSpeed/KM_H_FAKTOR) fakegpsValue = 0.0f;
                 fakegpsValue += (float)Math.random();
                 return fakegpsValue;
             }
@@ -139,6 +148,14 @@ public class MainActivity extends AppCompatActivity
             mUsername = savedInstanceState.getString("user");
         }
         configureButton();
+    }
+
+    private float round1Digit(float number) {
+        return Math.round(number*10f)/10f;
+    }
+
+    private float round3Digit(float number) {
+        return Math.round(number*1000f)/1000f;
     }
 
     @Override
@@ -216,6 +233,8 @@ public class MainActivity extends AppCompatActivity
                 } else {
                     Log.d(TAG, "odpalamy");
                     locationManager.requestLocationUpdates("gps", 0, 0, listener);
+                    timer1 = System.currentTimeMillis();
+                    record = false;
                 }
             }
         });
@@ -235,6 +254,37 @@ public class MainActivity extends AppCompatActivity
                 max.setText("max: " + maxSpeed);
             }
         });
+    }
+
+
+    public class LoadInitialDataTask extends AsyncTask<Void, Void, Boolean> {
+
+        LoadInitialDataTask() { }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            tvCarName.setText(loadCarName());
+            return true;
+        }
+
+        private String loadCarName() {
+            Car car = CarRepository.getDefaultCar(getApplicationContext());
+            if(car != null) {
+                return "Jedziesz " + car.getManufacturer() + " " +car.getModel();
+            }
+            return "";
+        }
+
+
+        @Override
+        protected void onPostExecute(final Boolean result) {
+            mcarTask = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            mcarTask = null;
+        }
     }
 }
 
